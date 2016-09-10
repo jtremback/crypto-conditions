b7029d752c4df2736ac8d69519d345fe01170661
Jehan <jehan@Jehans-MacBook.local>
2016-09-09 17:28:10 -0700

    in progress

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -7,6 +7,12 @@ import (
 	"errors"
 )
 
+// Regex for validating fulfillments
+//
+// This is a generic, future-proof version of the fulfillment regular
+// expression.
+const FULFILLMENT_REGEX = "/^cf:([1-9a-f][0-9a-f]{0,3}|0):[a-zA-Z0-9_-]*$/"
+
 // MakeUvarint returns a byte slice containing a uvarint
 func MakeUvarint(n uint64) []byte {
 	uvi := make([]byte, 10)
